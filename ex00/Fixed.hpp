e475e31dd0fb787d5e6c38d9f7e58b9b51292455
TrailOshell <TrailOshell.com>
2025-07-08 13:13:48 +0700

    ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 12:48:54 by tsomchan          #+#    #+#             */
+/*   Updated: 2025/07/08 13:03:07 by tsomchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int					_fpNum;
+		static const int	_fracBits;
+	public:
+		Fixed();
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+# ifndef DEBUG_MODE
+#  define DEBUG_MODE	0
+# endif
+
+# define NCL		"\033[0m"
+# define BLK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GRN		"\033[0;32m"
+# define YLW		"\033[0;33m"
+# define BLU		"\033[0;34m"
+# define PUR		"\033[0;35m"
+# define CYN		"\033[0;36m"
+# define WHT		"\033[0;37m"
+
+#endif
