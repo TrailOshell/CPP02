8f2b3796cd77557b49a555f1bba4864df59659b3
TrailOshell <TrailOshell.com>
2025-07-08 19:03:43 +0700

    ex01 init

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 18:33:01 by tsomchan          #+#    #+#             */
+/*   Updated: 2025/07/08 18:50:49y tsomchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::_fractBits = 8;
+
+Fixed::Fixed()
+{
+	std::cout << GRN "Default constructor called" NCL << std::endl;
+	_fpNum = 0;
+}
+
+Fixed::Fixed(const int number)
+{
+	std::cout << PUR"Int " GRN"constructor called" NCL << std::endl;
+	_fpNum = number;
+}
+
+Fixed::Fixed(const float number)
+{
+	std::cout << CYN"Float " GRN"constructor called" NCL << std::endl;
+	_fpNum = number;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << YLW "Copy constructor called" NCL << std::endl;
+	*this = fixed;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << RED "Destructor called" NCL << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << PUR "Copy assignment operator called" NCL << std::endl;
+	_fpNum = fixed.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << CYN "getRawBits member function called" NCL << std::endl;
+	return (_fpNum);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_fpNum = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)_fpNum);
+}
+
+int Fixed::toInt( void ) const
+{
+	return ((int)_fpNum);
+}
+
+std::ostream &operator<<(std::ostream &outStream, Fixed const &fixed)
+{
+	outStream << fixed.toFloat();
+	return (outStream);
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 18:33:14 by tsomchan          #+#    #+#             */
+/*   Updated: 2025/07/08 18:53:53 by tsomchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int					_fpNum;
+		static const int	_fractBits;
+	public:
+		Fixed();
+		Fixed(const int number);
+		Fixed(const float number);
+		Fixed(const Fixed &fixed);
+		~Fixed();
+		Fixed &operator=(const Fixed &fixed);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &fixed);
+
+# ifndef DEBUG_MODE
+#  define DEBUG_MODE	0
+# endif
+
+# define NCL		"\033[0m"
+# define BLK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GRN		"\033[0;32m"
+# define YLW		"\033[0;33m"
+# define BLU		"\033[0;34m"
+# define PUR		"\033[0;35m"
+# define CYN		"\033[0;36m"
+# define WHT		"\033[0;37m"
+
+#endif
